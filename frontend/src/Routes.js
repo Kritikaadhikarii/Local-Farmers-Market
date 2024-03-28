f8776fa22dc1be306954532dcebb5a16621d4a5b
Kritikaadhikarii <itskritikaaa@gmail.com>
2024-03-28 21:36:15 +0545

    bugs fixed

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,7 +1,7 @@
-import LoginPage from "../src/pages/LoginPage"
-import SignupPage from "../src/pages/SignupPage"
-import ActivationPage from "../src/pages/SignupPage"
-import HomePage from "../src/pages/HomePage"
+import LoginPage from "../src/pages/LoginPage";
+import SignupPage from "../src/pages/SignupPage";
+import ActivationPage from "../src/pages/SignupPage";
+import HomePage from "../src/pages/HomePage";
 
 export {
     LoginPage,
