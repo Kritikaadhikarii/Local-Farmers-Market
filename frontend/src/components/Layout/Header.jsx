b569c9ed723fb7003643c888cd1ba66fa883b98a
Kritikaadhikarii <itskritikaaa@gmail.com>
2024-03-30 21:59:52 +0545

    codes for main dashboard

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -3,6 +3,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import styles from "../../styles/styles";
 import { productData } from "../../static/data";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Header = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -13,8 +14,8 @@ const Header = () => {
     setSearchTerm(term);
 
     const filteredProducts = productData.filter((product) =>
-        product.name.toLowerCase().includes(term.toLowerCase())
-      );
+      product.name.toLowerCase().includes(term.toLowerCase())
+    );
     setSearchData(filteredProducts);
   };
 
@@ -51,41 +52,50 @@ const Header = () => {
             top-1.5 
             cursor-pointer"
           />
-          {
-          searchData && searchData.length !== 0 ? (
-              <div
-                className="absolute 
+          {searchData && searchData.length !== 0 ? (
+            <div
+              className="absolute 
                 min-h-[30vh]
                 bg-slate-50
                 shadow-sm-2
                 z[9]
                 p-4"
-              >
-                {
-                  searchData && searchData.map((i, index) => {
-                    const d = i.name;
-                    const Product_name = d.replace(/\s+/g, "-");
-                    return (
-                      <Link to = {`/product/${Product_name}`}>
-                        <div className="w-full flex items-start-py-3">
-                          <img src="{i.image_Url[0].url" alt="" 
+            >
+              {searchData &&
+                searchData.map((i, index) => {
+                  const d = i.name;
+                  const Product_name = d.replace(/\s+/g, "-");
+                  return (
+                    <Link to={`/product/${Product_name}`} key={index}>
+                      <div className="w-full flex items-start-py-3">
+                        <img
+                          src={i.image_Url[0].url}
+                          alt="image"
                           className="w-[40px] h-[40px] mr-[10px]"
-                          />
-                          <h1>{i.name}</h1>
-                        </div>
-                      </Link>
-                    )
-                  })
-                }
-              </div>
-            ) : null
-          }
+                        />
+                        <h1>{i.name}</h1>
+                      </div>
+                    </Link>
+                  );
+                })}
+            </div>
+          ) : null}
         </div>
 
         {/* new */}
-        {/* <div className=""> */}
+        <div className={`${styles.button}`}>
+          <Link to="/seller">
+            <h1 className="text-[#fff]
+            flex items-center">
+              Become Seller <IoIosArrowForward className="ml-1" />
+            </h1>
+          </Link>
+        </div>
 
-        {/* </div> */}
+        {/* neww */}
+        <div className={`${styles.section} relative ${styles.noramlFlex} justify-between`}>
+          <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block"></div>
+        </div>
       </div>
     </div>
   );
