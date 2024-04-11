543a78ee5776121ddac688fabe04205ac132ecc5
Kritikaadhikarii <itskritikaaa@gmail.com>
2024-04-11 21:22:35 +0545

    shop dashboard completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,14 +26,16 @@ import {
   ShopCreateEvents,
   ShopAllEvents,
   ShopAllCoupouns,
-  ShopPreviewPage
+  ShopPreviewPage,
+  ShopeHomePage
 } from "./routes/ShopRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Store from "./redux/store";
 import { loadSeller, loadUser } from "./redux/actions/user";
 import ProtectedRoute from "./routes/ProtectedRoute";
-import { ShopHomePage } from "./routes/ShopRoutes.js";
+// import { ShopHomePage } from "./routes/ShopRoutes";
+import { ShopHomePage } from "./pages/Shop/ShopHomePage.jsx"
 import SellerProtectedRoute from "./routes/SellerProtectedRoute";
 import { getAllProducts } from "./redux/actions/product";
 import { getAllEvents } from "./redux/actions/event";
