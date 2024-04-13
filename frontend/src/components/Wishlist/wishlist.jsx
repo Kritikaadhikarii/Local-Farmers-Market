1f1c61616a8f06c4a5b09069f24df9200f445358
Kritikaadhikarii <itskritikaaa@gmail.com>
2024-04-13 19:49:20 +0545

    wishlist added

--- frontend/src/components/Wishlist/wishlist.jsx
+++ frontend/src/components/Wishlist/wishlist.jsx
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
 import { RxCross1 } from "react-icons/rx";
-import { BsCartPlus } from "react-icons/bs";
+import { IoBagHandleOutline } from "react-icons/io5";
+import {BsCartPlus} from "react-icons/bs";
 import styles from "../../styles/styles";
+import { Link } from "react-router-dom";
 import { AiOutlineHeart } from "react-icons/ai";
 
 const Wishlist = ({ setOpenWishlist }) => {
-    const cartData = [
-        {
-          name: "Potato",
-          description: "Red potato",
-          price: 999,
-        },
-        {
-          name: "Potato",
-          description: "Red potato",
-          price: 459,
-        },
-        {
-          name: "Potato",
-          description: "Red potato",
-          price: 569,
-        },
-      ];
-      
+  const cartData = [
+    {
+      name: "Juice Carrot Orange Tumeric Organic, 32 Fl Oz",
+      description: "test",
+      price: 999,
+    },
+    {
+      name: "Juice Carrot Orange Tumeric Organic, 32 Fl Oz",
+      description: "test",
+      price: 245,
+    },
+    {
+      name: "Juice Carrot Orange Tumeric Organic, 32 Fl Oz",
+      description: "test",
+      price: 645,
+    },
+  ];
   return (
     <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
-      <div className="fixed top-0 right-0 h-full w-[30%] 300px:w-[25%] bg-white flex flex-col overflow-y-scroll justify-between shadow-sm">
-        <div className="w-full h-screen">
-          {/* Close button */}
-          <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
+      <div className="fixed top-0 right-0 min-h-full w-[25%] bg-white flex flex-col justify-between shadow-sm">
+        <div>
+          <div className="flex w-full justify-end pt-5 pr-5">
             <RxCross1
               size={25}
               className="cursor-pointer"
@@ -38,14 +38,14 @@ const Wishlist = ({ setOpenWishlist }) => {
           {/* Item length */}
           <div className={`${styles.noramlFlex} p-4`}>
             <AiOutlineHeart size={25} />
-            <h5 className="pl-2 text-[20px] font-[500]">4 items</h5>
+            <h5 className="pl-2 text-[20px] font-[500]">3 items</h5>
           </div>
-          {/* Cart Single Items */}
+
+          {/* cart Single Items */}
           <br />
           <div className="w-full border-t">
-            {cartData.map((i, index) => (
-              <CartSingle key={index} data={i} />
-            ))}
+            {cartData &&
+              cartData.map((i, index) => <CartSingle key={index} data={i} />)}
           </div>
         </div>
       </div>
@@ -54,25 +54,25 @@ const Wishlist = ({ setOpenWishlist }) => {
 };
 
 const CartSingle = ({ data }) => {
-  const [value] = useState(1);
+  const [value, setValue] = useState(1);
   const totalPrice = data.price * value;
+
   return (
-    <div className="border-b p-4 flex justify-between items-center">
+    <div className="border-b p-4">
       <div className="w-full flex items-center">
-        <RxCross1 className="cursor pointer" />
-        <img
-          src="https://www.alimentarium.org/sites/default/files/media/image/2017-02/AL027-01_pomme_de_terre_0_0.jpg"
-          alt=""
-          className="w-[130px] h-[80px] ml-2"
+        <RxCross1 className="cursor-pointer" />
+        <img src="https://m.media-amazon.com/images/I/51GSEiSyMgL._SL1000_.jpg" alt=""
+        className="w-[80px] h-[80px] ml-2"
         />
+      
         <div className="pl-[5px]">
-          <h1>{data.name}</h1>
-          <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
-            US${totalPrice}
-          </h4>
+            <h1>{data.name}</h1>
+            <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
+                US${totalPrice}
+            </h4>
         </div>
         <div>
-          <BsCartPlus size={20} className="cursor-pointer" tile="Add to cart" />
+            <BsCartPlus size={20} className="cursor-pointer" tile="Add to cart" />
         </div>
       </div>
     </div>
