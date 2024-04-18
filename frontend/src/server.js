bcb55272ae7e4aadd7bfe0db9ceb2b316b24967c
Kritikaadhikarii <itskritikaaa@gmail.com>
2024-04-18 08:34:02 +0545

    signup issues fixed

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,8 @@
-// export const server = "http://localhost:8000/api/v2";
-// export const backend_url = "http://localhost:8000/";
+export const server = "http://localhost:8000/api/v2";
+export const backend_url = "http://localhost:8000/";
 
-export const server = "http://[::1]:8000/api/v2";
-export const backend_url = "http://[::1]:8000/";
+// export const server = "http://[::1]:8000/api/v2";
+// export const backend_url = "http://[::1]:8000/";
+
+// export const server = "http://192.168.1.73:8000/api/v2";
+// export const backend_url = "http://192.168.1.73:8000/";
