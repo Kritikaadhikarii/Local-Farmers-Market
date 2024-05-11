a98467cbc2691a1b2e72c93d2a7995d270388122
Kritikaadhikarii <itskritikaaa@gmail.com>
2024-05-11 12:56:50 +0545

    components for order added

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,4 +1,4 @@
-import LoginPage from "../pages/Login";
+import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
 import ActivationPage from "../pages/ActivationPage";
 import HomePage from "../pages/HomePage";
